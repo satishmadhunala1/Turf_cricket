47e57b4d5e19e04148b1f4d086684eff7b87172d
satish <satishmadhunala03@gmail.com>
2025-06-23 23:30:40 +0530

    changes

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://turf-cricket-backend.onrender.com' });
 
 // Add a request interceptor to include the auth token
 API.interceptors.request.use((config) => {
