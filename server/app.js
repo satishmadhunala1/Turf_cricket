02e52d0e678abfcea3cbc6806e4052b9e293f7e0
satish <satishmadhunala03@gmail.com>
2025-06-24 10:09:39 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -16,7 +16,11 @@ const paymentRoutes = require('./routes/paymentRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://turf-cricket-frontend.onrender.com/",
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Database connection
